793c6aa66526aba0de7cfaa4afc4c0e39933dc9b
Jamie Hatfield <jamie.hatfield@parsons.com>
2023-10-17 12:41:52 +0000

    (app) Trying to access express using railway alias

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,7 +8,7 @@ import { BehaviorSubject, Observable, map } from 'rxjs';
 export class DataService {
     constructor(private http: HttpClient) { }
 
-    private URL = 'https://worth-watching-express.up.railway.app';
+    private URL = 'https://express';
 
     private lengthSubject = new BehaviorSubject<number>(0);
     public length$ = this.lengthSubject.asObservable();
