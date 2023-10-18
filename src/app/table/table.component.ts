8add5b4e0a5d28382c09e172dab9f64fb9b22d7e
Jamie Hatfield <jamie.hatfield@parsons.com>
2023-10-18 14:00:16 +0000

    (app) cleaning up some styles

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../data.service';
 import { GamesDataSource } from '../dataSource/gamesDataSource';
 import { MatPaginator } from '@angular/material/paginator';
-import { tap } from 'rxjs';
+import { merge, tap } from 'rxjs';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-table',
@@ -14,6 +15,7 @@ export class TableComponent implements OnInit {
   displayedColumns = ['week', 'date', 'awayTeamName', 'homeTeamName', 'ei'];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(public dataService: DataService) { }
 
@@ -23,11 +25,15 @@ export class TableComponent implements OnInit {
   }
 
   ngAfterViewInit() {
-    this.paginator.page
+    // reset the paginator after sorting
+    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+
+    merge(this.sort.sortChange, this.paginator.page)
       .pipe(tap(() => this.loadGamesPage())).subscribe();
   }
 
   loadGamesPage() {
+    // this will be fired every time the selected page changes or one of the filters changes
     this.dataSource.loadGames(-1, -1, undefined, undefined, undefined, this.paginator.pageIndex, this.paginator.pageSize);
   }
 
