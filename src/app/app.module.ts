7db11652516b26e0fd5bbe08977eff080cffb669
Jamie Hatfield <jamie.hatfield@parsons.com>
2023-10-19 15:03:17 +0000

    [ui] Starting filtering by conference

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -6,11 +6,33 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 import { MatInputModule} from '@angular/material/input';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginatorIntl, MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSortModule } from '@angular/material/sort';
 import { TableComponent } from './table/table.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { Subject } from 'rxjs';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
+
+@Injectable()
+export class CustomPaginatorIntl implements MatPaginatorIntl {
+  changes = new Subject<void>();
+  firstPageLabel = 'First page';
+  itemsPerPageLabel = '';
+  lastPageLabel = 'Last page';
+  nextPageLabel = 'Next page';
+  previousPageLabel = 'Previous page';
+
+  getRangeLabel(page: number, pageSize: number, length: number): string {
+    if (length === 0) return 'Page 1 of 1';
+    const amountPages = Math.ceil(length/pageSize);
+    return `Page ${page+1} of ${amountPages}`;
+  }
+}
 
 @NgModule({
   declarations: [
@@ -26,9 +48,15 @@ import { MatIconModule } from '@angular/material/icon';
     MatPaginatorModule,
     MatSortModule,
     MatProgressSpinnerModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgFor
   ],
-  providers: [],
+  providers: [{provide: MatPaginatorIntl, useClass: CustomPaginatorIntl}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -5,6 +5,9 @@ import { MatPaginator } from '@angular/material/paginator';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { Subject, merge, takeUntil, tap } from 'rxjs';
 import { MatSort } from '@angular/material/sort';
+import { Router } from '@angular/router';
+import { HttpParams } from '@angular/common/http';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-table',
@@ -17,15 +20,18 @@ export class TableComponent implements OnInit {
   destroyed = new Subject<void>();
   dataSource: GamesDataSource;
   displayedColumns = ['date', 'awayTeamName', 'homeTeamName', 'ei'];
+  filterOptions = ['Top 25', 
+                   'Power 5', 'ACC', 'Big 12', 'Big Ten', 'Pac-12', 'SEC', 'FBS Independents',
+                   'Group of 5', 'American Athletic', 'Conference USA', 'Mid-American', 'MWC', 'Sun Belt'];
+  filters = new FormControl('');
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor(public dataService: DataService, private breakpointObserver: BreakpointObserver) { }
+  constructor(public dataService: DataService, private breakpointObserver: BreakpointObserver, private router: Router) { }
 
   ngOnInit() {
     this.dataSource = new GamesDataSource(this.dataService);
-    this.dataSource.loadGames(-1, -1, 'top25', 'desc', 'ei'); // default to most recent year and week
     this.dataService.year$.pipe(takeUntil(this.destroyed)).subscribe(year => this.year = year);
     this.dataService.week$.pipe(takeUntil(this.destroyed)).subscribe(week => this.week = week);
     this.breakpointObserver.observe([Breakpoints.XSmall])
@@ -34,6 +40,9 @@ export class TableComponent implements OnInit {
         if (result.matches) this.displayedColumns = ['awayTeamName', 'homeTeamName', 'ei'];
         else this.displayedColumns = ['date', 'awayTeamName', 'homeTeamName', 'ei'];
       });
+    const httpParams = new HttpParams({fromString: window.location.search.substring(1)});
+    this.week = Number(httpParams.get('week') ?? '-1');
+    this.dataSource.loadGames(-1, this.week, 'Top 25', 'desc', 'ei'); // default to most recent year and week
   }
 
   ngAfterViewInit() {
@@ -45,8 +54,9 @@ export class TableComponent implements OnInit {
   }
 
   loadGamesPage() {
+    this.router.navigate([], {queryParams: { week: this.week }, queryParamsHandling: 'merge'})
     // this will be fired every time the selected page changes or one of the filters changes
-    this.dataSource.loadGames(this.year, this.week, 'top25', this.sort.direction, this.sort.active, this.paginator.pageIndex, this.paginator.pageSize);
+    this.dataSource.loadGames(this.year, this.week, 'Top 25', this.sort.direction, this.sort.active, this.paginator.pageIndex, this.paginator.pageSize);
   }
 
   prevWeek() { this.week--; this.paginator.pageIndex = 0; this.loadGamesPage(); }
